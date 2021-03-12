760ee9aacec6cd5acfd316517b8b15950a5740b3
buianhduck2@gmail.com <buianhduck2@gmail.com>
2021-03-12 10:31:29 +0700

    fist commit

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+      	<hr>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
